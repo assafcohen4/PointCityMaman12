e4c08513dd49f3cfd87e2ee2c3cdb425d1b50a56
assafcohen4 <assafcohen4@gmail.com>
2018-11-16 23:17:24 +0200

    Backup maman 12

---
+++ src/City.java
@@ -0,0 +1,146 @@
+public class City {
+    private String _cityName;
+    private Point _cityCenter;
+    private Point _centralStation;
+    private long _numOfResidents;
+    private int _noOfNeighborhoods;
+
+    /**
+     * creates a new City object
+     * @param cityName name of the city
+     * @param cityCenter a Point that represents the center of the city
+     * @param centralStation a Point that represents the central station of the city
+     * @param numOfResidents number of residents of the city
+     * @param noOfNeighborhoods number of neighborhoods in the city
+     */
+    public City(String cityName, Point cityCenter, Point centralStation, long numOfResidents, int noOfNeighborhoods){
+        this._cityName = cityName;
+        this._cityCenter = cityCenter;
+        this._centralStation = centralStation;
+        if (numOfResidents >= 0) {
+            this._numOfResidents = numOfResidents;
+        } else {
+            this._numOfResidents = 0;
+        }
+        if(noOfNeighborhoods >= 1)
+        this._noOfNeighborhoods = noOfNeighborhoods;
+        else{
+            this._noOfNeighborhoods = 0;
+        }
+    }
+
+    /**
+     * Copy constructor
+     * @param city city to be copied
+     *
+     */
+    public City(City city){
+        this._cityName = city._cityName;
+        this._cityCenter = city._cityCenter;
+        this._centralStation = city._centralStation;
+        this._numOfResidents = city._numOfResidents;
+        this._noOfNeighborhoods = city._noOfNeighborhoods;
+    }
+
+    /** gets the city name*/
+    public String getCityName(){
+        return this._cityName;
+    }
+    /** gets the city centre*/
+    public Point getCityCenter(){
+        return this._cityCenter;
+    }
+    /** gets the central station*/
+    public Point getCentralStation(){
+        return this._centralStation;
+    }
+    /** gets the number of residents*/
+    long getNumOfResidents(){
+        return this._numOfResidents;
+    }
+    /** gets the number of neighborhoods*/
+    int getNoOfNeighborhoods(){
+        return this._noOfNeighborhoods;
+    }
+
+    /** sets the city value
+     * @param cityName name to be given
+     */
+    void setCityName(String cityName){
+        this._cityName = cityName;
+    }
+
+    /** sets the city value
+     * @param cityCenter value to be set
+     */
+    void setCityCenter(Point cityCenter){
+        this._cityCenter = cityCenter;
+    }
+
+    /** sets the central station
+     * @param centralStation value to be set
+     */
+    void setCentralStation(Point centralStation){
+        this._centralStation = centralStation;
+    }
+
+    /** sets the city value
+     * @param numOfResidents value to be set
+     */
+    void setNumOfResidents(long numOfResidents){
+        this._numOfResidents = numOfResidents;
+    }
+    /** sets the number of neigborhoods
+     * @param noOfNeighborhoods value to be set
+     */
+    void setNoOfNeighborhoods(int noOfNeighborhoods){
+        this._noOfNeighborhoods = noOfNeighborhoods;
+    }
+
+    /**
+     * @return String that represents the city in the
+     * following format City Name, City Center,
+     * Central Station, Number of Residents, Number of Neighborhoods
+     */
+    public String toString() {
+       String format = "City Name: " + this.getCityName() + "\n"
+               + "City Center: " + this.getCityCenter() + "\n"
+               + "Central Station: " + this.getCentralStation() + "\n"
+               + "Number of Residents: " + this.getNumOfResidents() + "\n"
+               + "Number of Neighborhoods" + this.getNoOfNeighborhoods();
+
+       return format;
+    }
+
+    /**
+     * @param residentsUpdate  adds or removes residents from the city
+     * @return true if number of residents was updated, else false
+     */
+    public boolean addResidents(long residentsUpdate){
+        long newNumberOfresidents = this.getNumOfResidents() + residentsUpdate;
+        if(newNumberOfresidents >= 0){
+            this.setNumOfResidents(newNumberOfresidents);
+            return true;
+        }
+        this.setNumOfResidents(0);
+        return false;
+    }
+
+    public void moveCentralStation(double deltaX, double deltaY){
+
+
+        Point newStationLocation = new Point(this.getCentralStation().getX() + deltaX, this.getCentralStation().getY() + deltaY);
+
+        if(!(newStationLocation.getX() < 0 || newStationLocation.getY() < 0)) {
+            this.setCentralStation(newStationLocation);
+        }
+    }
+
+    public double distanceBetweenCenterAndStation(){
+
+    }
+
+    public City newCity(String name, double dX, double dY){
+
+    }
+}
---
+++ src/Point.java
@@ -0,0 +1,143 @@
+/**
+ * Class represents a point in two dimensional space and defines method that
+ * determine the spacial relation between two points in space
+ * @author Assaf Cohen
+ *
+ */
+
+public class Point {
+
+    private double _radius;
+    private double _alpha;
+
+    //constructors:
+    /**
+     * creates a new Point object
+     * @param x vertical position of the point on a cartesian grid
+     * @param y horizontal position of the point on a cartesian grid
+     */
+
+    public Point(double x, double y){
+        if(x < 0 && y < 0){
+           this._radius = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+           this._alpha = Math.atan(y/x) * (180/Math.PI);
+        }
+
+    }
+
+    /**
+     * Copy constructor
+     * @param other point to be copied
+     *
+     */
+    public Point(Point other){
+        this._radius = other._radius;
+        this._alpha = other._alpha;
+    }
+
+    /** gets x value of point  */
+    public double getX(){
+        return _radius * (Math.cos(_alpha) * (180/Math.PI));
+    }
+
+    /** gets y value of point  */
+    public double getY(){
+        return _radius * (Math.sin(_alpha) * (180/Math.PI));
+    }
+
+    /** sets x value
+     * @param num the value to be set
+     */
+    public void setX(double num){
+
+        if(num>= 0){
+          this._radius = Math.sqrt(Math.pow(num,2) + Math.pow(this.getY(),2));
+          this._alpha = Math.acos(num/this._radius) * (180/Math.PI) ;
+        }
+    }
+
+    /** sets y value
+     * @param num the value to be set
+     */
+    public void setY(double num){
+        if(num>= 0){
+            this._radius = Math.sqrt(Math.pow(num,2) + Math.pow(this.getX(),2));
+            this._alpha = Math.asin(num/this._radius) * (180/Math.PI) ;
+        }
+    }
+
+    /**
+     * @return String that represents the point
+     * in the following format:
+     * (x-value, y-value)
+     */
+    public String toString(){
+        return "(" + this.getX() + "," + this.getY() + ")";
+    }
+
+    /**
+     * checks if the point's properties are the same as that of the point given as parameter
+     * @param other point to be equated against
+     * @retun true iff the point and the parameter have the same property values
+     */
+    public boolean equals(Point other){
+        return this._radius == other._radius && this._alpha == other._alpha;
+    }
+
+    /**
+     * checks if the point is above the parameter point on a cartesian grid
+     * @param other point to be evaluated against
+     * @retun true iff the point is above the parameter point
+     */
+    public boolean isAbove(Point other){
+        return this.getY() > other.getY();
+    }
+
+    /**
+     * checks if the point is under the parameter point on a cartesian grid
+     * @param other point to be evaluated against
+     * @retun true iff the point is under the parameter point
+     */
+    public boolean isUnder(Point other){
+        return !this.isAbove(other) && this.getY() != other.getY() ;
+    }
+
+    /**
+     * checks if the point is to the left of the parameter point on a cartesian grid
+     * @param other point to be evaluated against
+     * @retun true iff the point is to the left of the parameter point
+     */
+    public boolean isLeft(Point other){
+        return this.getX() < other.getX();
+    }
+
+    /**
+     * checks if the point is to the right of the parameter point on a cartesian grid
+     * @param other point to be evaluated against
+     * @retun true iff the point is to the right of the parameter point
+     */
+    public boolean isRight(Point other){
+        return !isLeft(other) && this.getX() != other.getX();
+    }
+
+    /**
+     * checks the distance between two points
+     * @param p point to be evaluated against
+     * @retun the distance between the two points
+     */
+    public double distance(Point p){
+        return Math.sqrt(Math.pow(p.getY() - this.getY(),2)+ Math.pow((p.getX() - this.getX()),2));
+    }
+
+    /**
+     * checks the distance between two points
+     * @param dx distance by which the point would move on the x axis
+     * @param dy distance by which the point would move on the y axis
+     */
+    public void move(double dx, double dy){
+        if(!(this.getX() + dx < 0 || this.getY()+ dy < 0)) {
+            this.setX(this.getX() + dx);
+            this.setY(this.getY() + dy);
+        }
+    }
+}
